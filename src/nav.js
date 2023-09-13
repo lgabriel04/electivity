7205382e7902ab8b7b314bb840c8a869e9a5e7f0
lgabriel04 <110146300+lgabriel04@users.noreply.github.com>
2023-09-14 00:00:04 +0800

    Update nav.js
    jgjh

--- src/nav.js
+++ src/nav.js
@@ -6,7 +6,7 @@ import './nav.css';
 const Nav = () => {
   return (
     <nav className="navbar" style={{ backgroundColor: '#D9D9D9' }}>
-      <h1>SAVE YOUR NOTES | SYN</h1>
+      <h1>SAVE YOUR NOTES !| SYN</h1>
       <Link className='im-bored-button' to="/memes">I'm Bored</Link> {/* Add a Link to the Memes page */}
     </nav>
   );
